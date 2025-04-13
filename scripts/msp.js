1b8df56d74ed78efbf2322cef873ed3d528c43d6
omcho1989 <purkissete21@gmail.com>
2025-04-13 12:41:40 -0600

    finishing touches

--- scripts/msp.js
+++ scripts/msp.js
@@ -3,3 +3,16 @@ document.querySelector("#currentyear").innerHTML = `&copy; ${currentYear}`;
 
 const lastModified = document.lastModified;
 document.querySelector("#lastmodified").textContent = lastModified;
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.querySelector('.menu-button');
+    const nav = document.querySelector('nav');
+
+    if (menuButton && nav) {
+        menuButton.addEventListener('click', () => {
+            console.log('Menu button clicked');
+            menuButton.style.display = 'none'; 
+            nav.classList.add('open'); 
+        });
+    } 
+});
